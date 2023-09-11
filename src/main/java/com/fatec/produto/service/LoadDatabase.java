28669c67c45451a8e8efce76bd074800b3a70582
yuritakamidev <yuri.takamisawa.ribeiro@gmail.com>
2023-09-11 15:48:39 -0300

    atualizando Loadatabase

--- src/main/java/com/fatec/produto/service/LoadDatabase.java
+++ src/main/java/com/fatec/produto/service/LoadDatabase.java
@@ -11,35 +11,25 @@ import com.fatec.produto.model.IprodutoRepository;
 import com.fatec.produto.model.Produto;
 import java.time.LocalDate;
 
-
 import com.fatec.produto.model.IclienteRepository;
 import com.fatec.produto.model.Cliente;
-
-
 
 @Configuration
 public class LoadDatabase {
 
+    // Somente os produtos serão lançados automaticamente no banco de dados, o resto
+    // terá que ser inserido na medida que houver requests, como criar usuario,
+    // fazer pedido.
     @Bean
     CommandLineRunner initProdutoDatabase(IprodutoRepository produtoRepository) {
         return args -> {
             List<Produto> produtos = Arrays.asList(
-                new Produto("Pera", "Fruta", "No Ponto", 4.5, 130, LocalDate.parse("2023-07-15")),
-                new Produto("Banana", "Fruta", "Maduro", 1.0, 150, LocalDate.parse("2023-08-31")),
-                new Produto("Mamão", "Fruta", "Maduro", 10.0, 100, LocalDate.parse("2023-10-10")));
+                    new Produto("Pera", "Fruta", "No Ponto", 4.5, 130, LocalDate.parse("2023-07-15")),
+                    new Produto("Banana", "Fruta", "Maduro", 1.0, 150, LocalDate.parse("2023-08-31")),
+                    new Produto("Mamão", "Fruta", "Maduro", 10.0, 100, LocalDate.parse("2023-10-10")));
 
             produtoRepository.saveAll(produtos);
         };
     }
 
-    @Bean
-    CommandLineRunner initClienteDatabase(IclienteRepository clienteRepository) {
-        return args -> {
-            List<Cliente> clientes = Arrays.asList(
-                new Cliente("João", "Rua A", "joao@email.com", "123456789"),
-                new Cliente("Maria", "Rua B", "maria@email.com", "987654321"));
-
-            clienteRepository.saveAll(clientes);
-        };
-    }
 }
