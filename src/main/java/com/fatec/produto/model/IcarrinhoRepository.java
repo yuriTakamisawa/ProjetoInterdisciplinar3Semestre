c969f8f6bfe768b87d60faf9b0c64a34f2f4bd55
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2023-09-26 08:39:24 -0300

    ajustando clienterepository

--- src/main/java/com/fatec/produto/model/IcarrinhoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fatec.produto.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IcarrinhoRepository extends JpaRepository<Carrinho, Long> {
-    // métodos
-}
--- src/main/java/com/fatec/produto/model/IclienteRepository.java
+++ src/main/java/com/fatec/produto/model/IclienteRepository.java
@@ -2,6 +2,7 @@ package com.fatec.produto.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@repository
 public interface IclienteRepository extends JpaRepository<Cliente, Long> {
     public Cliente findAllByEmail(String email);
 
